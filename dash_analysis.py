07dbb39fdca39403db31ee6db2101619618a5a73
murilomm192 <murilomm192@gmail.com>
2019-03-04 21:30:53 -0400

    data

---
+++ dash_analysis.py
@@ -0,0 +1,14 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html 
+from dash.dependencies import Input, Output
+import numpy as np 
+import pandas as pd 
+
+s = pd.read_csv('data.csv')
+
+
+
+print(s)
+
+
